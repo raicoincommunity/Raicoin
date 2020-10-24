0b54e6a68c042ffe416710089dd9ea11aa4f5a93
BitHaru <bitharu7@gmail.com>
2020-10-24 14:50:06 +0900

    modify cmakefile for macOS wallet

--- rai/secure/plat/darwin/plat.cpp
+++ rai/secure/plat/darwin/plat.cpp
@@ -3,6 +3,32 @@
 #include <mach-o/dyld.h>
 #include <limits.h>
 #include <boost/filesystem.hpp>
+#include <pwd.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+boost::filesystem::path rai::AppPath()
+{
+    auto entry(getpwuid(getuid()));
+    boost::filesystem::path result(entry->pw_dir);
+    return result;
+}
+
+void rai::SetStdinEcho(bool enable)
+{
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    if (enable)
+    {
+        tty.c_lflag |= ECHO;
+    }
+    else
+    {
+        tty.c_lflag &= ~ECHO;
+    }
+    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
 
 std::string rai::PemPath()
 {
